f1fdf89097c99712126b91d8b29977bb79d7d644
nayshins <jnations@8thlight.com>
2015-03-17 14:07:33 -0500

    Add set function

--- src/grid.ml
+++ src/grid.ml
@@ -1,18 +1,16 @@
 open Core.Std
 open Token
 
-let empty = ""
-
 let new_board =
-  [|EMPTY; EMPTY; EMPTY;
+  [EMPTY; EMPTY; EMPTY;
     EMPTY; EMPTY; EMPTY;
-    EMPTY; EMPTY; EMPTY|]
+    EMPTY; EMPTY; EMPTY]
 
 let get_token board index =
-  Array.get board index
+  List.nth_exn board index
 
 let set_token board index token =
-  let board = board in
-  Array.set board index token
-    board
+  let grid_array = Array.of_list board in
+  Array.set grid_array index token;
+  Array.to_list grid_array
 
--- test/gridTest.ml
+++ test/gridTest.ml
@@ -3,24 +3,26 @@ open OUnit2
 open Token
 
 let test1 test_ctxt = assert_equal true true
-let empty_board test_ctxt = assert_equal [|EMPTY;EMPTY;EMPTY;
-                                           EMPTY;EMPTY;EMPTY;
-                                           EMPTY;EMPTY;EMPTY;|] Grid.new_board
+let empty_board test_ctxt = assert_equal [EMPTY;EMPTY;EMPTY;
+                                          EMPTY;EMPTY;EMPTY;
+                                          EMPTY;EMPTY;EMPTY;] Grid.new_board
 
 let get_token_test test_ctxt =
   let board = Grid.new_board in
-  assert_equal EMPTY (Grid.get_token board 0)
+  let token = Grid.get_token board 0 in
+  assert_equal EMPTY token
 
-let set_token_test test_ctxt =
-  let board = Grid.new_board in
-  assert_equal [|X;EMPTY;EMPTY;
-                 EMPTY;EMPTY;EMPTY;
-                 EMPTY;EMPTY;EMPTY;|] (Grid.set_token board 0 X)
+let set_token_test test_ctxt = 
+  let board = Grid.new_board in 
+  assert_equal [X;EMPTY;EMPTY;
+                EMPTY;EMPTY;EMPTY;
+                EMPTY;EMPTY;EMPTY;] (Grid.set_token board 0 X)
 
 let tests = 
   "suite">:::
   ["test true">:: test1;
-   "new board returns a board with all EMPTY">:: empty_board;
+   "new board returns a board with all EMPTY tokens">:: empty_board;
    "get_token returns the token at a specific index">:: get_token_test;
-   "set_token returns an array with updated value">:: set_token_test ]
-
+   "set_token returns a new list with token inserted at index" >::
+   set_token_test;
+  ]
