64703cd6f500059f9736095e3d642ea4eb6d57a8
nayshins <jnations@8thlight.com>
2015-03-19 12:21:38 -0500

    Add to_string function to token module

--- src/token.ml
+++ src/token.ml
@@ -1,3 +1,9 @@
 open Core.Std
 
 type token = X | O | EMPTY of int
+
+let to_string token =
+  match token with
+  | X -> "X"
+  | O -> "O"
+  | EMPTY _  -> " "
--- test/tokenTest.ml
+++ test/tokenTest.ml
@@ -23,9 +23,30 @@ let test_empty test_ctxt =
     ~ctxt:test_ctxt
     true (match_token (EMPTY 0))
 
+let test_to_string_X test_ctxt =
+  assert_equal
+    ~ctxt:test_ctxt
+    "X"
+    (Token.to_string X)
+
+let test_to_string_O test_ctxt =
+  assert_equal
+    ~ctxt:test_ctxt
+    "O"
+    (Token.to_string O)
+
+let test_to_string_EMPTY test_ctxt =
+  assert_equal
+    ~ctxt:test_ctxt
+    " "
+    (Token.to_string (EMPTY 0))
 let tests =
   "suite" >:::
   ["X is a valid token" >:: test_x;
    "O is a valid token" >:: test_o;
-   "EMPTY is a valid token" >:: test_empty]
+   "EMPTY is a valid token" >:: test_empty;
+   "to_string X returns 'X'" >:: test_to_string_X;
+   "to_string O returns 'O'" >:: test_to_string_O;
+   "to_string EMPTY returns ' '" >:: test_to_string_EMPTY;
+  ]
 
