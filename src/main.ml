0d3da1fb8b9a3c370fb70b09c113f6f7c4cfe7ac
nayshins <jnations@8thlight.com>
2015-03-21 12:36:51 -0500

    Reduce negamax depth limit

--- src/main.ml
+++ src/main.ml
@@ -3,11 +3,12 @@ open Player
 open Game
 open Human
 open SimpleComputer
+open Negamax
 
 let () =
   let module Player1 = PlayerX in
   let module Player2 = PlayerO in
-  let module Computer = SimpleComputer in
+  let module Computer = Negamax in
   let module Human = Human in
   let module Game = Game (Player1 (Computer))
       (Player2 (Human)) in
