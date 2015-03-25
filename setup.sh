6b232d18696814bc9eff71f162f8dc2cb113ecd2
nayshins <jnations@8thlight.com>
2015-03-25 11:19:13 -0500

    Add setup shell script

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#inspired by thoughtbot laptop script
+
+fancy_echo() {
+  local fmt="$1"; shift
+
+  # shellcheck disable=SC2059
+  printf "\n$fmt\n" "$@"
+}
+
+brew_install_or_upgrade() {
+  if brew_is_installed "$1"; then
+    if brew_is_upgradable "$1"; then
+      fancy_echo "Upgrading %s ..." "$1"
+      brew upgrade "$@"
+    else
+      fancy_echo "Already using the latest version of %s. Skipping ..." "$1"
+    fi
+  else
+    fancy_echo "Installing %s ..." "$1"
+    brew install "$@"
+  fi
+}
+
+brew_install_or_upgrade 'ocaml'
+brew_install_or_upgrade 'opam'
+
+opam init
+eval `opam config env`
+
+opam install core ounit
+
